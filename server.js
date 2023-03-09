6d0f1dbb343cd96bf01911873706f27271d0f153
AshS <emailash62@gmail.com>
2023-03-09 14:59:35 +0800

    created db schema

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const express = require('express');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 
 const PORT = process.env.PORT || 3002;
 
-
-
-
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-}
+  app.listen(PORT, () => console.log("Now listening"));
+});
