a976314a565b2720126fb35e8e66f0cf391346d8
AshS <emailash62@gmail.com>
2023-03-09 14:43:46 +0800

    created package.json file, added dependencies, created server.js as entry point and required packages. Opened port, created a .env file and created a .gitnore to exclude the .env.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+
+const PORT = process.env.PORT || 3002;
+
+
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+}
